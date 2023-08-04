fc7e0a972b0bfa5220d9c1c6afea74b07ddab8e4
Yan Palmer <86450167+YanPalmer@users.noreply.github.com>
2023-08-03 21:16:17 -0300

    Aprendendo a usar TypeScript

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,68 @@
+/*
+class Pessoa {
+    nome: string;
+    idade: number;
 
+    constructor(nome: string, idade: number) {
+        this.nome = nome;
+        this.idade = idade;
+    }
 
+    apresentar() {
+        console.log(`Olá, ${this.nome}. Sua idade é: ${this.idade}`);
+    };
+}
 
+const pessoa1 = new Pessoa("Yan", 23);
+console.log(pessoa1.nome, pessoa1.idade);
+pessoa1.apresentar();
+*/
 
+/*
+class Pessoa {
+    nome: string;
+    private idade: number;
+
+    constructor(nome: string, idade: number) {
+        this.nome = nome;
+        this.idade = idade;
+    }
+
+    getIdade() {
+        return this.idade;
+    }
+
+    setIdade(idade: number) {
+        this.idade = idade;
+    }
+}
+
+const pessoa1 = new Pessoa("Yan", 23);
+console.log(pessoa1.idade);
+*/
+
+class ContaBancaria {
+    private saldo: number;
+    divida: number;
+
+    constructor(saldo: number) {
+        this.saldo = saldo;
+        this.divida = 1000;
+    }
+
+    getSaldo() {
+        return this.saldo;
+    }
+
+    setSaldo(saldo: number) {
+        this.saldo = saldo + this.saldo;
+        console.log("O saldo foi alterado");
+    }
+}
+
+const conta1 = new ContaBancaria(1500);
+//conta1.saldo = conta1.saldo - 50;   // Não funciona pois está privado
+
+console.log(conta1);
+conta1.setSaldo(1000);
+console.log(conta1);
