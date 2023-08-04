f11a902e7a97cabf86cb32f83e936520a5ce2bec
Yan Palmer <86450167+YanPalmer@users.noreply.github.com>
2023-08-03 23:34:18 -0300

    Atividade quase concluída
    Falta aprender usar o get e o set

--- README.md
+++ README.md
@@ -15,3 +15,113 @@ Crie métodos getters e setters para acessar e alterar o valor da idade, respeit
 * Crie uma classe chamada "Animal" com um método chamado "falar".
 Crie classes específicas que herdem de "Animal" (por exemplo: "Cachorro", "Gato", etc.)
 e sobrescrevam o método "falar" para exibir o som característico de cada animal.
+
+<!--
+/*  Código escrito na aula para entender os conceitos
+class Pessoa {
+    nome: string;
+    idade: number;
+
+    constructor(nome: string, idade: number) {
+        this.nome = nome;
+        this.idade = idade;
+    }
+
+    apresentar() {
+        console.log(`Olá, ${this.nome}. Sua idade é: ${this.idade}`);
+    };
+}
+
+const pessoa1 = new Pessoa("Yan", 23);
+console.log(pessoa1.nome, pessoa1.idade);
+pessoa1.apresentar();
+*/
+
+/*
+class Pessoa {
+    nome: string;
+    private idade: number;
+
+    constructor(nome: string, idade: number) {
+        this.nome = nome;
+        this.idade = idade;
+    }
+
+    getIdade() {
+        return this.idade;
+    }
+
+    setIdade(idade: number) {
+        this.idade = idade;
+    }
+}
+
+const pessoa1 = new Pessoa("Yan", 23);
+console.log(pessoa1.idade);
+*/
+
+/*
+class ContaBancaria {
+    private saldo: number;
+    divida: number;
+
+    constructor(saldo: number) {
+        this.saldo = saldo;
+        this.divida = 1000;
+    }
+
+    getSaldo() {
+        return this.saldo;
+    }
+
+    setSaldo(saldo: number) {
+        this.saldo = saldo + this.saldo;
+        console.log("O saldo foi alterado");
+    }
+}
+
+const conta1 = new ContaBancaria(1500);
+//conta1.saldo = conta1.saldo - 50;   // Não funciona pois está privado
+
+console.log(conta1);
+conta1.setSaldo(1000);
+console.log(conta1);
+*/
+
+
+/*
+//  Código desenvolvido por mim para aprendizado
+
+//  Classe automóvel
+class automóvel {
+    //  Parâmetros
+    nome: string;
+    cor: string;
+
+    //  Método construtor
+    constructor(nome: string, cor: string) {
+        //  Variáveis
+        this.nome = nome;
+        this.cor = cor;
+    }
+}
+const automóvel1 = new automóvel("Avião", "vermelho");
+console.log(automóvel1);
+
+
+//  Extensão de automóvel para carro
+class carro extends automóvel {
+    rodas: number;
+    placa: string;
+
+    //  Herda os parâmetros NOME e COR da classe automóvel ++ os novos parâmetros RODAS e PLACA
+    constructor(nome: string, cor: string, rodas: number, placa: string) {
+        super(nome, cor);
+        this.rodas = rodas;
+        this.placa = placa;
+    }
+}
+const carro1 = new carro("COMANDER", "preto", 4, "YAN-1234");
+console.log(carro1);
+*/
+-->
--- src/index.ts
+++ src/index.ts
@@ -1,68 +1,68 @@
 /*
+//  Código desenvolvido por mim para aprendizado
+
+//  Classe automóvel
+class automóvel {
+    //  Parâmetros
+    nome: string;
+    cor: string;
+
+    //  Método construtor
+    constructor(nome: string, cor: string) {
+        //  Variáveis
+        this.nome = nome;
+        this.cor = cor;
+    }
+}
+const automóvel1 = new automóvel("Avião", "vermelho");
+console.log(automóvel1);
+
+
+//  Extensão de automóvel para carro
+class carro extends automóvel {
+    rodas: number;
+    placa: string;
+
+    //  Herda os parâmetros NOME e COR da classe automóvel ++ os novos parâmetros RODAS e PLACA
+    constructor(nome: string, cor: string, rodas: number, placa: string) {
+        super(nome, cor);
+        this.rodas = rodas;
+        this.placa = placa;
+    }
+}
+const carro1 = new carro("COMANDER", "preto", 4, "YAN-1234");
+console.log(carro1);
+*/
+
+//  Atividade
+
 class Pessoa {
     nome: string;
-    idade: number;
-
+    private idade: number;
     constructor(nome: string, idade: number) {
         this.nome = nome;
         this.idade = idade;
     }
-
     apresentar() {
-        console.log(`Olá, ${this.nome}. Sua idade é: ${this.idade}`);
-    };
+        console.log(`Bem vindo ${this.nome}. Vejo que você tem ${this.idade} anos.`)
+    }
 }
-
 const pessoa1 = new Pessoa("Yan", 23);
-console.log(pessoa1.nome, pessoa1.idade);
-pessoa1.apresentar();
-*/
+// pessoa1.apresentar();
 
-/*
-class Pessoa {
-    nome: string;
-    private idade: number;
-
-    constructor(nome: string, idade: number) {
-        this.nome = nome;
-        this.idade = idade;
+class Aluno extends Pessoa {
+    matrícula: string;
+    constructor(nome: string, idade: number, matrícula: string) {
+        super(nome, idade);
+        this.matrícula = matrícula;
+    }
+    apresentar() {
+        console.log(`Bem vindo ${this.nome}. Vejo que você tem ${this.idade} anos. A sua matrícula é: ${this.matrícula}`);
     }
 
     getIdade() {
         return this.idade;
     }
-
-    setIdade(idade: number) {
-        this.idade = idade;
-    }
 }
-
-const pessoa1 = new Pessoa("Yan", 23);
-console.log(pessoa1.idade);
-*/
-
-class ContaBancaria {
-    private saldo: number;
-    divida: number;
-
-    constructor(saldo: number) {
-        this.saldo = saldo;
-        this.divida = 1000;
-    }
-
-    getSaldo() {
-        return this.saldo;
-    }
-
-    setSaldo(saldo: number) {
-        this.saldo = saldo + this.saldo;
-        console.log("O saldo foi alterado");
-    }
-}
-
-const conta1 = new ContaBancaria(1500);
-//conta1.saldo = conta1.saldo - 50;   // Não funciona pois está privado
-
-console.log(conta1);
-conta1.setSaldo(1000);
-console.log(conta1);
+const aluno1 = new Aluno("Anna", 22, "abc0A");
+aluno1.getIdade();
