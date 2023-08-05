fe54d8f08eabe3e5bf720b514162632fef506fc0
Yan Palmer <86450167+YanPalmer@users.noreply.github.com>
2023-08-05 20:45:31 -0300

    Atividade da sala de aula concluída
    Criei uma classe chamada animal e criei sub-classes chamadas Cachorro, Gato e Vaca que reproduzem seus respectivos sons

--- src/index.ts
+++ src/index.ts
@@ -34,8 +34,11 @@ const carro1 = new carro("COMANDER", "preto", 4, "YAN-1234");
 console.log(carro1);
 */
 
-//  Atividade
+/* ======================================= */
 
+//  Atividade 01
+
+console.log("Atividade 01");
 class Pessoa {
     nome: string;
     private idade: number;
@@ -44,11 +47,17 @@ class Pessoa {
         this.idade = idade;
     }
     apresentar() {
-        console.log(`Bem vindo ${this.nome}. Vejo que você tem ${this.idade} anos.`)
+        console.log(`Bem vindo ${this.nome}. Vejo que você tem ${this.idade} anos.`);
+    }
+    getIdade() {
+        return this.idade;
+    }
+    setIdade(idade: number) {
+        this.idade = idade;
     }
 }
 const pessoa1 = new Pessoa("Yan", 23);
-// pessoa1.apresentar();
+pessoa1.apresentar();
 
 class Aluno extends Pessoa {
     matrícula: string;
@@ -56,13 +65,56 @@ class Aluno extends Pessoa {
         super(nome, idade);
         this.matrícula = matrícula;
     }
-    apresentar() {
-        console.log(`Bem vindo ${this.nome}. Vejo que você tem ${this.idade} anos. A sua matrícula é: ${this.matrícula}`);
+}
+const aluno1 = new Aluno("Anna", 22, "A0005");
+console.log(aluno1.getIdade()); // Idade atual
+aluno1.setIdade(14)             // Altera a idade
+console.log(aluno1.getIdade());      // Mostra a idade atual de aluno1
+console.log();
+
+
+
+//  Atividade 02
+
+console.log("Atividade 02")
+class Animal {
+    nomeAnimal: string;
+    somAnimal: string;
+    constructor(nomeAnimal: string, somAnimal: string) {
+        this.nomeAnimal = nomeAnimal;
+        this.somAnimal = somAnimal;
     }
 
-    getIdade() {
-        return this.idade;
+    fala() {
+        console.log(`O ${this.nomeAnimal} faz: ${this.somAnimal}`)
     }
 }
-const aluno1 = new Aluno("Anna", 22, "abc0A");
-aluno1.getIdade();
+
+
+class Cachorro extends Animal {
+
+    constructor(nomeAnimal: string, somAnimal: string) {
+        super(nomeAnimal, somAnimal)
+    }
+}
+const animal1 = new Cachorro("Toby", "Au au");  // Declara o nome e o som do animal
+animal1.fala()
+
+
+class Gato extends Animal {
+    
+    constructor(nomeAnimal: string, somAnimal: string) {
+        super(nomeAnimal, somAnimal)
+    }
+}
+const animal2 = new Gato("Gato", "Miau");
+animal2.fala();
+
+class Vaca extends Animal {
+
+    constructor(nomeAnimal: string, somAnimal: string) {
+        super(nomeAnimal, somAnimal)
+    }
+}
+const animal3 = new Vaca("Vaca", "Muuuu");
+animal3.fala();
